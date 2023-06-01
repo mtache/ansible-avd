cfeaf71114fdb2cfe0e48aa58cb0a2c01a855669
Carl Baillargeon <carl.baillargeon@arista.com>
2023-06-01 14:52:12 -0400

    Moved device interaction to ANTA

--- ansible_collections/arista/avd/plugins/action/anta_runner.py
+++ ansible_collections/arista/avd/plugins/action/anta_runner.py
@@ -6,7 +6,7 @@ import json
 
 from ansible.errors import AnsibleActionFail
 from ansible.plugins.action import ActionBase
-from anta.inventory.models import InventoryDevice
+from anta.inventory.models import InventoryDeviceHttpApi
 from anta.loader import parse_catalog
 from anta.result_manager import ResultManager
 from yaml import safe_load
@@ -25,22 +25,17 @@ class ActionModule(ActionBase):
 
         # Need to set the right become_context to the _connection.httpapi object
         connection.set_become(play_context)
-        """
-        NOTE: For now, these parameters are needed to instantiate InventoryDevice
-        but only "name" and "session" are actually used by the class instance when using httpapi connection.
-        """
+
         device_params = {
             "name": task_vars["inventory_hostname"],
-            "host": task_vars["ansible_host"],
-            "username": task_vars["ansible_user"],
-            "password": task_vars["ansible_httpapi_password"],
-            "port": 443,
             "session": connection,
         }
 
         try:
-            anta_device = InventoryDevice(**device_params)
+            # Creating the ANTA device object with the HttpApi interface
+            anta_device = InventoryDeviceHttpApi(**device_params)
 
+            # Creating the catalog from Ansible task
             catalog_path = self._task.args.get("catalog_path")
             with open(catalog_path, "r", encoding="UTF-8") as file:
                 input_catalog = safe_load(file)
@@ -53,14 +48,8 @@ class ActionModule(ActionBase):
             results = ResultManager()
 
             for test_class, test_params in tests_catalog:
-                # from_ansible is a new attribute to run the appropriate decorator depending if the class is instantiated from Ansible or not.
-                test_instance = test_class(device=anta_device, from_ansible=True)
-
-                # NOTE: This part will be integrated in ANTA when it will support an httpapi session object.
-                for index, command in enumerate(test_instance.instance_commands):
-                    response = anta_device.session.send_request(command.command, version=command.version, output=command.ofmt)
-                    data = json.loads(response) if command.ofmt == "json" else response
-                    test_instance.instance_commands[index].output = data
+                # sync is a new attribute to run ANTA tests synchronously.
+                test_instance = test_class(device=anta_device, sync=True)
 
                 # Tests requiring input parameters will extract them from task_vars.
                 # If ALL parameters are not found in task_vars, tests will run with the parameters from the input_catalog file.
@@ -69,13 +58,11 @@ class ActionModule(ActionBase):
                     extracted_params = test_class.extract_parameters(task_vars)
                     if not all(v is None for v in extracted_params.values()):
                         results.add_test_result(test_instance.test(**extracted_params))
-                    else:
-                        results.add_test_result(test_instance.test(**test_params))
-                # Run remaining catalog tests that don't require any parameters
                 else:
-                    results.add_test_result(test_instance.test())
+                    results.add_test_result(test_instance.test(**test_params))
 
             result["changed"] = True
+            # FIXME: Need to parse the results for the report
             result["results"] = json.loads(results.get_results(output_format="json"))
 
         except Exception as e:
