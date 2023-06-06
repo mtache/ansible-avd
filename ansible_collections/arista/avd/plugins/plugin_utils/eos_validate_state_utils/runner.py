9bb0881dea035f46ed904b002d3409c740b64eee
Carl Baillargeon <carl.baillargeon@arista.com>
2023-06-06 13:35:33 -0400

    Added annotations and docstring to runner.py

--- ansible_collections/arista/avd/plugins/plugin_utils/eos_validate_state_utils/runner.py
+++ ansible_collections/arista/avd/plugins/plugin_utils/eos_validate_state_utils/runner.py
@@ -3,9 +3,26 @@ from __future__ import annotations
 import asyncio
 import json
 
+from anta.result_manager import ResultManager, TestResult
 
-# TODO: Add annotations, docstring
-async def run_tests(manager, device, tests):
+from ansible_collections.arista.avd.plugins.plugin_utils.eos_validate_state_utils.models import InventoryDeviceHttpApi
+
+
+async def run_tests(manager: ResultManager, device: InventoryDeviceHttpApi, tests: list[tuple[callable[..., TestResult], dict]]) -> dict:
+    """
+    Asynchronously runs tests on a device and gathers results.
+
+    Args:
+      manager (ResultManager): An instance of ANTA ResultManager to manage test results.
+      device (InventoryDeviceHttpApi): An instance of the custom implementation of ANTA InventoryDevice with Ansible HttpApi connection session object.
+      tests (list[tuple]): A list of tuples, where each tuple contains a callable test function and a dictionary of test parameters.
+
+    Returns:
+      dict: Test result data in a dictionary format.
+
+    This function runs each test on the provided device asynchronously.
+    Results are gathered and added to the ResultManager. The results are then outputted in JSON format and converted to a data dictionary.
+    """
     tasks = [test_class(device=device).test(**test_params) for test_class, test_params in tests]
 
     results_list = await asyncio.gather(*tasks)
